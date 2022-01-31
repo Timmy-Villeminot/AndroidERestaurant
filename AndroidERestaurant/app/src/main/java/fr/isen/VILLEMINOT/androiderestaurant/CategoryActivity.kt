c9ee35bb7398499b68ce7653616c49339e23cb48
Timmy-Villeminot <timmy.villeminot@isen.yncrea.fr>
2022-01-31 13:22:15 +0100

    Suite

--- AndroidERestaurant/app/src/main/java/fr/isen/VILLEMINOT/androiderestaurant/CategoryActivity.kt
+++ AndroidERestaurant/app/src/main/java/fr/isen/VILLEMINOT/androiderestaurant/CategoryActivity.kt
@@ -2,20 +2,68 @@ package fr.isen.VILLEMINOT.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SimpleAdapter
+import androidx.recyclerview.widget.RecyclerView
 import fr.isen.VILLEMINOT.androiderestaurant.databinding.ActivityCategoryActvityBinding
 
 enum class LunchType {
     STARTER, MEAL, DESSERT;
+
+    companion object {
+        fun getResString(type: LunchType): Int {
+            return when(type) {
+                STARTER -> R.string.starters
+                MEAL -> R.string.meals
+                DESSERT -> R.string.desserts
+            }
+        }
+    }
 }
 
 class CategoryActivity : AppCompatActivity() {
     lateinit var binding: ActivityCategoryActvityBinding
+    lateinit var currentCategory: LunchType
+
+    val fakeItems = listOf("Plat1", "Plat2", "Plat3", "Plat4")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCategoryActvityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val item: LunchType = intent.getSerializableExtra(HomeActivity.CategoryType) as? LunchType ?: LunchType.STARTER
-        binding.textView.setText("Contenu du get Extra")
+        currentCategory = intent.getSerializableExtra(HomeActivity.CategoryType) as? LunchType ?: LunchType.STARTER
+        setupTitle()
+        Log.d("life cycle", "CategoryActivity onCreate")
+    }
+
+    override fun onDestroy() {
+        Log.d("life cycle", "CategoryActivity onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("life cycle", "CategoryActivity onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life cycle", "CategoryActivity onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life cycle", "CategoryActivity onStart")
+    }
+
+
+    override fun onStop() {
+        Log.d("life cycle", "CategoryActivity onStop")
+        super.onStop()
+    }
+
+    private fun setupTitle() {
+        binding.Title.text = getString(LunchType.getResString(currentCategory))
     }
 }
--- AndroidERestaurant/app/src/main/java/fr/isen/VILLEMINOT/androiderestaurant/HomeActivity.kt
+++ AndroidERestaurant/app/src/main/java/fr/isen/VILLEMINOT/androiderestaurant/HomeActivity.kt
@@ -3,6 +3,7 @@ package fr.isen.VILLEMINOT.androiderestaurant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import fr.isen.VILLEMINOT.androiderestaurant.databinding.ActivityHomeBinding
 
@@ -14,6 +15,8 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         listenClick()
+
+        Log.d("Life cycle", "HomeActivity onCreate")
     }
 
     private fun listenClick() {
@@ -31,6 +34,32 @@ class HomeActivity : AppCompatActivity() {
         val intent = Intent(this, CategoryActivity::class.java)
         intent.putExtra(HomeActivity.CategoryType, item)
         startActivity(intent)
+    }
+
+    override fun onDestroy() {
+        Log.d("life cycle", "HomeActivity onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("life cycle", "HomeActivity onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("life cycle", "HomeActivity onResume")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("life cycle", "HomeActivity onStart")
+    }
+
+
+    override fun onStop() {
+        Log.d("life cycle", "HomeActivity onStop")
+        super.onStop()
     }
 
     companion object {
